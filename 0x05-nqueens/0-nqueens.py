0e260b92f2f42cf1756471a54c6dc6ee1d81fe42
kurvs <kavssimply@gmail.com>
2024-08-27 18:23:52 +0000

    n queen using backtracking

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -14,3 +14,21 @@ def is_safe(board, row, col):
            board[i] + i == col + row:
             return False
     return True
+
+def solve_nqueens(N):
+    """Solve the N-Queens problem and return all solutions."""
+    def backtrack(row=0):
+        if row == N:
+            solution = [[i, board[i]] for i in range(N)]
+            solutions.append(solution)
+            return
+        for col in range(N):
+            if is_safe(board, row, col):
+                board[row] = col
+                backtrack(row + 1)
+                board[row] = -1
+
+    board = [-1] * N
+    solutions = []
+    backtrack()
+    return solutions
