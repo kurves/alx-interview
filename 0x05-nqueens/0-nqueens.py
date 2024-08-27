980b042c69d17c05006592978562619142a70589
kurvs <kavssimply@gmail.com>
2024-08-27 18:27:09 +0000

    n queens backtracking

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -32,3 +32,24 @@ def solve_nqueens(N):
     solutions = []
     backtrack()
     return solutions
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_nqueens(N)
+    print_solutions(solutions)
+
+if __name__ == "__main__":
+    main()
