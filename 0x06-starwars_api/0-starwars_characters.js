1eb1c055b6dc4dbdc40142611791cb2aa75fe815
kurves <kavssimply@gmail.com>
2024-09-06 00:38:57 -0700

    maintain character order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,20 +2,22 @@
 
 const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
 const request = require('request');
+
 
 request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, (err, resp, body) => {
-        if (!err && resp.statusCode === 200) {
-          console.log(JSON.parse(body).name);
-        }
+
+    // Use Promise.all to ensure the order of the character responses
+    const characterPromises = characters.map((characterUrl) => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, (err, resp, body) => {
+          if (!err && resp.statusCode === 200) {
+            resolve(JSON.parse(body).name);
+          } else {
+            reject(err);
+          }
+        });
       });
     });
-  } else {
-    console.error('Error:', error);
-  }
-});
