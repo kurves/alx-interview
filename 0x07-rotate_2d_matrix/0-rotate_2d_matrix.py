8c310bebf6b3fc74e40a5a03cc1b3eae91d35ef8
kurves <kavssimply@gmail.com>
2024-09-10 07:34:43 +0000

    Rotate 2D matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+module to rotate a 2 by 2 matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    funtion to rotate a two by two matrix
+    """
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        matrix[i].reverse()
