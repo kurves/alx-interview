09664e448d3fd00b48a53a7062399a4adb1e793d
kurves <kavssimply@gmail.com>
2024-09-27 06:39:46 +0000

    function that returns the perimeter of the island described in grid

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module defines the function island_perimeter that calculates
+the perimeter of an island described in a grid.
+"""
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island in grid.
+    int: Perimeter of the island
+    """
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
